bfc292c826ca7357443fd5624f29cb08a01cda5c
pavan <dpkdiwanji@gmail.com>
2025-08-26 20:06:47 +0530

    tester code updated

--- resources/views/admin/account_details.blade.php
+++ resources/views/admin/account_details.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <h1 class="mb-4">Account Details</h1>
+        <h1 class="mb-4">Account Details testet</h1>
 
         @if (session('success'))
             <div class="alert alert-success">
